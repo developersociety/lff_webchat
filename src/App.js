90765f1b16468530dd2ba3882dfc67639d2459c4
Martin Coote <martin@martincoote.com>
2022-02-03 14:50:11 +0000

    initial configuration

--- src/App.js
+++ src/App.js
@@ -12,6 +12,39 @@ class App extends React.Component {
     FlexWebChat.Manager.create(configuration)
       .then(manager => this.setState({ manager }))
       .catch(error => this.setState({ error }));
+
+    // change header text and hide the logo
+    FlexWebChat.MainHeader.defaultProps.titleText = "Chat With Live Fear Free";
+    FlexWebChat.MainHeader.defaultProps.showImage = false;
+
+    // hide welcome message blocks
+    FlexWebChat.MessagingCanvas.defaultProps.showWelcomeMessage = false;
+
+    // inital message
+    FlexWebChat.MessagingCanvas.defaultProps.predefinedMessage = {
+      body: `Croeso i Llinell Gymorth Byw Heb Ofn
+
+      Welcome to The Live Fear Free Helpline
+
+      Diolch am gysylltu gyda’r Llinell Gymorth. Sut gallwn i eich helpu chi heddiw?
+
+      Thank you for contacting the Helpline. How can we help you today?`,
+      authorName: "Helpline/Llinell Gymorth"
+    }
+
+    FlexWebChat.MessagingCanvas.defaultProps.memberDisplayOptions = {
+      yourDefaultName: 'Anonymous',
+      theirDefaultName: 'Helpline/Llinell Gymorth',
+      yourFriendlyNameOverride: false,
+      theirFriendlyNameOverride: false
+    }
+
+
+    // Open the chat window by default
+    FlexWebChat.Actions.invokeAction("ToggleChatVisibility");
+
+    // Restart the chat when the page is refreshed
+    FlexWebChat.Actions.invokeAction("RestartEngagement");
   }
 
   render() {
