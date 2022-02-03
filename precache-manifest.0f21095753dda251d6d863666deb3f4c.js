864078f096bbd1898fff8b67a30f0d3e361bbe6c
tomkins <tomkins@users.noreply.github.com>
2022-02-03 16:59:07 +0000

    deploy: 084dfffb4efdf2cd7a205fee1b9b79102612b40d

---
+++ precache-manifest.0f21095753dda251d6d863666deb3f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580f5037628de6d56c70e77958907d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e5071aa4fa58c89df9",
+    "url": "/static/css/main.ef53e09e.chunk.css"
+  },
+  {
+    "revision": "483e27b4ebb4c5d43b9b",
+    "url": "/static/js/2.eebc07cc.chunk.js"
+  },
+  {
+    "revision": "878f959acf21c1c89e8af12b216b7978",
+    "url": "/static/js/2.eebc07cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e5071aa4fa58c89df9",
+    "url": "/static/js/main.2910a8db.chunk.js"
+  },
+  {
+    "revision": "86389fed35a3bb5a420d",
+    "url": "/static/js/runtime-main.274e4769.js"
+  }
+]);
