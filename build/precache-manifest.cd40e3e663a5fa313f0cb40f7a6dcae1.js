f0b08e1128bb42d1bcfdc66a3f660d4d98b8c883
Martin Coote <martin@martincoote.com>
2022-02-03 15:24:40 +0000

    commit build folder

---
+++ build/precache-manifest.cd40e3e663a5fa313f0cb40f7a6dcae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee27c3f2fa1b2ecef9af4fb2a251f9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6463b89debee273addb1",
+    "url": "/static/css/main.ef53e09e.chunk.css"
+  },
+  {
+    "revision": "8a99ba7f5207e391b956",
+    "url": "/static/js/2.d7e8f391.chunk.js"
+  },
+  {
+    "revision": "878f959acf21c1c89e8af12b216b7978",
+    "url": "/static/js/2.d7e8f391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6463b89debee273addb1",
+    "url": "/static/js/main.dfa9206b.chunk.js"
+  },
+  {
+    "revision": "86389fed35a3bb5a420d",
+    "url": "/static/js/runtime-main.274e4769.js"
+  }
+]);
