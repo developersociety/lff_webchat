3cd41df41f4e3db56e208f7b5bd25c8d29a7742b
martincoote <martincoote@users.noreply.github.com>
2022-02-03 17:08:51 +0000

    deploy: 533c45fca670b2f366ab22a36d1abe73a5387855

---
+++ assets/webchat-appConfig.js
@@ -0,0 +1,9 @@
+var appConfig = {
+    //  change the your AccountSid
+    accountSid: "ACfd4a204f779532d9a78459ecc2bc0299",
+    // change to your Flex Flow SID
+    flexFlowSid: "FO7428c14391520d516712e4772a83bccd",
+    colorTheme: {
+        overrides: brandedColors
+    }
+}
