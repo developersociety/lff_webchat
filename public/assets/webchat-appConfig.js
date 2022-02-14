8882e018defcacb5fbc7768b0b6166b61569d5a0
Martin Coote <martin@martincoote.com>
2022-02-14 14:41:57 +0000

    add gdpr statement

--- public/assets/webchat-appConfig.js
+++ public/assets/webchat-appConfig.js
@@ -5,5 +5,38 @@ var appConfig = {
     flexFlowSid: "FO7428c14391520d516712e4772a83bccd",
     colorTheme: {
         overrides: brandedColors
-    }
+    },
+    startEngagementOnInit: false,
+    preEngagementConfig: {
+        description: "Data privacy",
+        fields: [
+            {
+                label: `In the course of our chat with you, you may choose to disclose sensitive personal data to us. We treat anything you tell us with the utmost confidentiality in accordance with our privacy policy, but we do need your consent to process it.
+
+                By that, we mean send it through our website in chat messages and keep a secure record of the chat, so that we know what information and support we gave you. Without your consent, we cannot start a chat with you.
+
+                Once a conversation has begun you'll receive responses by highly trained, professional support workers. You will be listened to, believed and offered help and support. You will not be judged or blamed and you don't have to take any further action if you don't feel ready to.
+
+                This is a safe and confidential space for you to talk, however, there may be instances when we may need to breach confidentiality. These are in circumstances when we feel there may be a significant danger to children or yourself and referrals would be made to the most appropriate agencies.
+
+                If you are in immediate danger please contact the police on 999 or 101.
+
+                Please select 'I agree' to say you give consent for us to process your data in accordance with this policy.
+`,
+                type: "SelectItem",
+                attributes: {
+                    name: "privacy",
+                    required: true
+                },
+                options: [
+                    {
+                        value: "yes",
+                        label: "I agree",
+                        selected: false
+                    },
+                ]
+            }
+        ],
+        submitLabel: "Start chat"
+    },
 }
